ca6f253a8a604b9f463c77e385074a8131b34e73
Santosh Khanal <117138852+santoshkhanal12@users.noreply.github.com>
2023-11-01 14:06:26 +0545

    Now added the individual codes
    added the chipering letters

--- password_generator/src/main.rs
+++ password_generator/src/main.rs
@@ -4,14 +4,114 @@ fn coding(mut plain_text:String,iterations: i32){
    let mut count=0;
    let mut encoded_text: String = "".to_owned();
 
-   while count<=iterations{
+   while count<iterations{
       println!("Print {} iterations",count);
      for letter in plain_text.chars(){ 
      if letter=='a'{
+      let mut addcoded: &str="a";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='b'{
       let mut addcoded: &str="b";
       encoded_text.push_str(addcoded);
      }
-      
+     if letter=='c'{
+      let mut addcoded: &str="f";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='d'{
+      let mut addcoded: &str="k";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='e'{
+      let mut addcoded: &str="g";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='f'{
+      let mut addcoded: &str="c";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='g'{
+      let mut addcoded: &str="d";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='h'{
+      let mut addcoded: &str="h";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='i'{
+      let mut addcoded: &str="l";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='j'{
+      let mut addcoded: &str="p";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='k'{
+      let mut addcoded: &str="u";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='l'{
+      let mut addcoded: &str="q";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='m'{
+      let mut addcoded: &str="m";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='n'{
+      let mut addcoded: &str="i";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='o'{
+      let mut addcoded: &str="e";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='p'{
+      let mut addcoded: &str="j";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='q'{
+      let mut addcoded: &str="n";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='r'{
+      let mut addcoded: &str="r";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='s'{
+      let mut addcoded: &str="v";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='t'{
+      let mut addcoded: &str="z";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='u'{
+      let mut addcoded: &str="w";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='v'{
+      let mut addcoded: &str="s";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='w'{
+      let mut addcoded: &str="o";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='x'{
+      let mut addcoded: &str="t";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='y'{
+      let mut addcoded: &str="x";
+      encoded_text.push_str(addcoded);
+     }
+     if letter=='z'{
+      let mut addcoded: &str="y";
+      encoded_text.push_str(addcoded);
+     }
+         
    }
 
      count=count+1;
@@ -24,13 +124,114 @@ fn coding(mut plain_text:String,iterations: i32){
 fn decoding(mut coded_text: String,iterations: i32){
    let mut count=0;
    let mut decoded_text: String = "".to_owned();
-   while count<=iterations{
+   while count<iterations{
       println!("Print {} iterations",count);
       for letter in coded_text.chars(){ 
          if letter=='a'{
-          let mut adddecoded: &str="b";
-          decoded_text.push_str(adddecoded);
-         }
+            let mut addcoded: &str="a";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='b'{
+            let mut addcoded: &str="b";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='c'{
+            let mut addcoded: &str="f";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='d'{
+            let mut addcoded: &str="g";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='e'{
+            let mut addcoded: &str="o";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='f'{
+            let mut addcoded: &str="c";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='g'{
+            let mut addcoded: &str="e";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='h'{
+            let mut addcoded: &str="h";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='i'{
+            let mut addcoded: &str="n";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='j'{
+            let mut addcoded: &str="p";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='k'{
+            let mut addcoded: &str="d";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='l'{
+            let mut addcoded: &str="i";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='m'{
+            let mut addcoded: &str="m";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='n'{
+            let mut addcoded: &str="q";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='o'{
+            let mut addcoded: &str="w";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='p'{
+            let mut addcoded: &str="j";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='q'{
+            let mut addcoded: &str="l";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='r'{
+            let mut addcoded: &str="r";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='s'{
+            let mut addcoded: &str="v";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='t'{
+            let mut addcoded: &str="x";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='u'{
+            let mut addcoded: &str="k";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='v'{
+            let mut addcoded: &str="s";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='w'{
+            let mut addcoded: &str="u";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='x'{
+            let mut addcoded: &str="y";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='y'{
+            let mut addcoded: &str="z";
+            decoded_text.push_str(addcoded);
+           }
+           if letter=='z'{
+            let mut addcoded: &str="t";
+            decoded_text.push_str(addcoded);
+           }
+               
       }   
      count=count+1;
    
